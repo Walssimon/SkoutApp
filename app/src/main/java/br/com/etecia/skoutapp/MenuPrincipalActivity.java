21b142e41e25b44ea967887b6da58dadf671aba7
Walssimon <walssimon123@gmail.com>
2022-09-08 22:09:37 -0300

    atualizacao 3

--- app/src/main/java/br/com/etecia/skoutapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/skoutapp/MenuPrincipalActivity.java
@@ -2,15 +2,28 @@ package br.com.etecia.skoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
-
+    Button btnvoltar;
     EditText edtmenuprincipal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
+
+        btnvoltar = findViewById(R.id.voltar);
+
+        btnvoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
     }
 }
