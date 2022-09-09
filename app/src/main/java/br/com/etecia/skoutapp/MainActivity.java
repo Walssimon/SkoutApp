ac65d9376ddf7dd7331115382107458d482befaa
Walssimon <walssimon123@gmail.com>
2022-09-08 22:05:17 -0300

    atualizacao 3

--- app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.etecia.skoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,17 +37,29 @@ public class MainActivity extends AppCompatActivity {
                 usuario = edtUsuario.getText().toString();
                 senha = edtSenha.getText().toString();
 
-                if (usuario.equals("etecia") && senha.equals("etecia")) {
 
-                } else {
-                    Toast.makeText(getApplicationContext(), "Usuario ou senha incorretos", Toast.LENGTH_SHORT).show();
-                    edtUsuario.setText("");
-                    edtSenha.setText("");
-                    edtUsuario.requestFocus();
-                }
 
             }
         });
+    btnEntrar.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String usuario,senha;
+            usuario = edtUsuario.getText().toString();
+            senha = edtSenha.getText().toString();
+            if (usuario.equals("etecia") && senha.equals("etecia")) {
+                startActivity(new Intent(getApplicationContext(),MenuPrincipalActivity.class));
+                finish();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Usuario ou senha incorretos", Toast.LENGTH_SHORT).show();
+                edtUsuario.setText("");
+                edtSenha.setText("");
+                edtUsuario.requestFocus();
+            }
+            Toast.makeText(getApplicationContext(),"Vou entrar, espera!",Toast.LENGTH_SHORT).show();
+        }
+    });
     }
 
 }
